f453c947863ed75e9b452ea90cc3fe3fbedeabac
Sammy Migiro <mwati.rvp@gmail.com>
2015-06-30 13:29:13 +0000

    Changes made by Sammy Migiro (mwati.rvp@gmail.com).
    Here is a test push of the very first changes in the tutorial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,12 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "flat"
+        });
+        $("#images").kendoMobileListView({
+            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+            template: "<img src='#: data #'>"
+        });
         navigator.splashscreen.hide();
     });
 }());
